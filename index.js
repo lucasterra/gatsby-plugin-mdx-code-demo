2266ba0120e7b69506de86e4d54b2231dee2f529
lucasterra <lucasterra7@gmail.com>
2020-07-30 18:00:55 +0200

    refactor: make code demo simpler

--- index.js
+++ index.js
@@ -1,125 +1,86 @@
+/* eslint-disable no-restricted-syntax */
 // @ts-nocheck
 /* eslint no-param-reassign: ["off"] */
 
 const fs = require('fs');
-const { promisify } = require('util');
+const { sync: mkdirp } = require('mkdirp');
 const crypto = require('crypto');
-const { join, dirname } = require('path');
+const { join } = require('path');
 const visit = require('unist-util-visit');
 const normalizePath = require('normalize-path');
-const highlightCode = require('./highlight-code');
 
-const readFile = promisify(fs.readFile);
+const CACHE_LOCATION = 'code-demo-imports';
 
-const CODE_DEMO_URL_REGEX = /^demo:\/\/(.+)/;
-
-function convertNodeToLocalDemo(node, parent, code, componentName) {
-  const highlightedCode = highlightCode('jsx', code);
-
-  delete parent.children;
-  parent.type = 'jsx';
-  parent.value = `<DemoComponent code={${highlightedCode}}><${componentName}/></DemoComponent>`;
+function md5(str) {
+  return crypto
+    .createHash('md5')
+    .update(str)
+    .digest('hex');
 }
 
-function fileExists(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.stat(filePath, (err, stats) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      resolve(stats.isFile());
-    });
-  });
+function convertNodeToInlineDemo(code, componentName, lang, position) {
+  return {
+    type: 'jsx',
+    value: `<CodeDemo code={${JSON.stringify(code)}} lang="${lang}"><${componentName}/></CodeDemo>`,
+    position,
+  };
 }
 
-function addSuffix(path, suffix) {
-  if (!path.endsWith(suffix)) {
-    return path + suffix;
-  }
+function createTemporaryFile(
+  code,
+  componentName,
+  cachePath,
+  componentsMap,
+) {
+  const filePath = normalizePath(join(cachePath, `${componentName}.js`));
+  fs.writeFileSync(filePath, code, { encoding: 'utf-8' });
 
-  return path;
+  componentsMap.set(filePath, componentName);
+
+  return filePath;
 }
 
-module.exports = async (
-  { markdownAST, markdownNode },
-  { demoComponent } = {},
+function createImportAST(name, filePath) {
+  return {
+    type: 'import',
+    value: `import ${name} from '${filePath}'`,
+  };
+}
+
+module.exports = (
+  { markdownAST, markdownNode, cache },
+  opts = {}, // eslint-disable-line no-unused-vars
 ) => {
-  if (!demoComponent) {
-    throw Error('Required CODEDEMO option "demoComponent" not specified');
-  }
-
-  const demoComponentPath = addSuffix(normalizePath(demoComponent), '.js');
-  if (!(await fileExists(demoComponentPath))) {
-    throw Error(
-      `Invalid CODEDEMO "demoComponent" specified "${demoComponentPath}"`,
-    );
-  }
-
   // Components which will get injected at the top of MDX file
-  const injectedComponentsHash = {
-    [demoComponentPath]: 'DemoComponent',
-  };
+  const componentsMap = new Map();
+  const cachePath = normalizePath(
+    join(cache.directory, CACHE_LOCATION),
+  );
+  mkdirp(cachePath);
 
-  const directory = normalizePath(dirname(markdownNode.fileAbsolutePath));
-  const getFilePath = (path) => {
-    let filePath = addSuffix(path, '.js');
-    filePath = normalizePath(join(directory, filePath));
+  const nodeIdHash = md5(markdownNode.id);
 
-    return filePath;
-  };
-
-  const nodesToChange = [];
-
-  // iterate inlineCode to find our regex
-  visit(markdownAST, 'link', (node, index, parent) => {
-    const match = node.url.match(CODE_DEMO_URL_REGEX);
-
-    if (match) {
-      const filePath = getFilePath(match[1]);
-      const filePathHash = crypto
-        .createHash('md5')
-        .update(filePath)
-        .digest('hex');
-
-      const componentName = `Demo_${filePathHash}`;
-
-      nodesToChange.push({
-        node,
-        parent,
-        filePath,
-        componentName,
-      });
-      injectedComponentsHash[filePath] = componentName;
+  let idx = 0;
+  visit(markdownAST, 'code', (node, index, parent) => {
+    const metaParams = (node.meta || '').split(/\s+/);
+    if (metaParams.indexOf('codeDemo') === -1) {
+      return;
     }
+
+    const { position, value: code, lang = 'js' } = node;
+    const componentName = `LocalComponent_${nodeIdHash}_${idx}`;
+
+    // update node to an mdx component
+    parent.children[index] = convertNodeToInlineDemo(code, componentName, lang, position);
+    createTemporaryFile(code, componentName, cachePath, componentsMap);
+    idx += 1;
   });
 
-  await Promise.all(
-    nodesToChange.map(async (n) => {
-      if (!(await fileExists(n.filePath))) {
-        throw Error(
-          `Invalid CODEDEMO link specified; no such file "${n.filePath}"`,
-        );
-      }
-
-      const code = await readFile(n.filePath, 'utf8');
-      convertNodeToLocalDemo(n.node, n.parent, code, n.componentName);
-    }),
-  );
-
-  const injectedComponents = Object.keys(injectedComponentsHash).map(
-    (filePath) => ({
-      type: 'import',
-      value: `import ${
-        injectedComponentsHash[filePath]
-      } from '${filePath.replace(`${directory}/`, './')}'`,
-    }),
-  );
-
-  if (injectedComponents.length > 1) {
-    markdownAST.children = [...injectedComponents, ...markdownAST.children];
+  const importAST = [];
+  for (const [componentName, filePath] of componentsMap.entries()) {
+    importAST.push(createImportAST(filePath, componentName));
   }
 
+  markdownAST.children = [...importAST, ...markdownAST.children];
   return markdownAST;
 };
