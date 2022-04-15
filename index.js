aa8ebd84f4eeb4c1e2ef4a944b70e23a2a23b80d
lucasterra <lucasterra7@gmail.com>
2022-04-15 16:52:33 +0200

    make it work as a plain remark plugin instead of gatsby

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable no-restricted-syntax */
 // @ts-nocheck
 /* eslint no-param-reassign: ["off"] */
@@ -5,33 +6,56 @@
 const fs = require('fs');
 const { sync: mkdirp } = require('mkdirp');
 const crypto = require('crypto');
-const { join } = require('path');
+const { join, isAbsolute } = require('path');
 const visit = require('unist-util-visit');
 const normalizePath = require('normalize-path');
+const os = require('os');
 
 const CACHE_LOCATION = 'code-demo-imports';
 
+let tmpDir;
+function createTempDir() {
+  if (tmpDir) {
+    return tmpDir;
+  }
+  tmpDir = fs.mkdtempSync(join(os.tmpdir(), CACHE_LOCATION));
+  return tmpDir;
+}
+
 function md5(str) {
-  return crypto
-    .createHash('md5')
-    .update(str)
-    .digest('hex');
+  return crypto.createHash('md5').update(str).digest('hex');
 }
 
 function convertNodeToInlineDemo(code, componentName, lang, position) {
+  // return {
+  //   type: 'jsx',
+  //   value: `<CodeDemo code={${JSON.stringify(
+  //     code,
+  //   )}} lang="${lang}"><${componentName}/></CodeDemo>`,
+  //   position,
+  // };
   return {
-    type: 'jsx',
-    value: `<CodeDemo code={${JSON.stringify(code)}} lang="${lang}"><${componentName}/></CodeDemo>`,
+    type: 'mdxJsxFlowElement',
+    name: 'CodeDemo',
+    attributes: [
+      { type: 'mdxJsxAttribute', name: 'code', value: JSON.stringify(code) },
+      { type: 'mdxJsxAttribute', name: 'lang', value: lang },
+    ],
+    children: [
+      {
+        type: 'mdxJsxFlowElement',
+        name: componentName,
+        attributes: [],
+        children: [],
+        data: { _mdxExplicitJsx: true },
+      },
+    ],
     position,
+    data: { _mdxExplicitJsx: true },
   };
 }
 
-function createTemporaryFile(
-  code,
-  componentName,
-  cachePath,
-  componentsMap,
-) {
+function createTemporaryFile(code, componentName, cachePath, componentsMap) {
   const filePath = normalizePath(join(cachePath, `${componentName}.js`));
   fs.writeFileSync(filePath, code, { encoding: 'utf-8' });
 
@@ -41,46 +65,90 @@ function createTemporaryFile(
 }
 
 function createImportAST(name, filePath) {
+  // return {
+  //   type: 'import',
+  //   value: `import ${name} from '${filePath}'`,
+  // };
   return {
-    type: 'import',
-    value: `import ${name} from '${filePath}'`,
+    type: 'mdxjsEsm',
+    value: `import ${name} from '${filePath}';`,
+    data: {
+      estree: {
+        type: 'Program',
+        body: [
+          {
+            type: 'ImportDeclaration',
+            specifiers: [
+              {
+                type: 'ImportDefaultSpecifier',
+                local: {
+                  type: 'Identifier',
+                  name,
+                },
+              },
+            ],
+            source: {
+              type: 'Literal',
+              value: filePath,
+              raw: `'${filePath}'`,
+            },
+          },
+        ],
+        sourceType: 'module',
+        comments: [],
+      },
+    },
   };
 }
 
-module.exports = (
-  { markdownAST, markdownNode, cache },
-  opts = {}, // eslint-disable-line no-unused-vars
-) => {
-  // Components which will get injected at the top of MDX file
-  const componentsMap = new Map();
-  const cachePath = normalizePath(
-    join(cache.directory, CACHE_LOCATION),
-  );
-  mkdirp(cachePath);
+function remarkCodeDemo(opts = {}) {
+  const rootDir = opts.rootDir || process.cwd();
 
-  const nodeIdHash = md5(markdownNode.id);
-
-  let idx = 0;
-  visit(markdownAST, 'code', (node, index, parent) => {
-    const metaParams = (node.meta || '').split(/\s+/);
-    if (metaParams.indexOf('codeDemo') === -1) {
-      return;
-    }
-
-    const { position, value: code, lang = 'js' } = node;
-    const componentName = `LocalComponent_${nodeIdHash}_${idx}`;
-
-    // update node to an mdx component
-    parent.children[index] = convertNodeToInlineDemo(code, componentName, lang, position);
-    createTemporaryFile(code, componentName, cachePath, componentsMap);
-    idx += 1;
-  });
-
-  const importAST = [];
-  for (const [componentName, filePath] of componentsMap.entries()) {
-    importAST.push(createImportAST(filePath, componentName));
+  if (!isAbsolute(rootDir)) {
+    throw new Error('"rootDir" has to be an absolute path');
   }
 
-  markdownAST.children = [...importAST, ...markdownAST.children];
-  return markdownAST;
-};
+  const cacheDirectory = createTempDir();
+
+  return function transformer(tree, file) {
+    // console.dir(tree, { depth: 100 });
+    // Components which will get injected at the top of MDX file
+    const componentsMap = new Map();
+    const cachePath = normalizePath(cacheDirectory);
+    mkdirp(cachePath);
+
+    const nodeIdHash = md5(file.value);
+
+    let idx = 0;
+    visit(tree, 'code', (node, index, parent) => {
+      const metaParams = (node.meta || '').split(/\s+/);
+      if (metaParams.indexOf('codeDemo') === -1) {
+        return;
+      }
+
+      const { position, value: code, lang = 'js' } = node;
+      const componentName = `LocalComponent_${nodeIdHash}_${idx}`;
+
+      // update node to an mdx component
+      parent.children[index] = convertNodeToInlineDemo(
+        code,
+        componentName,
+        lang,
+        position,
+      );
+      createTemporaryFile(code, componentName, cachePath, componentsMap);
+      idx += 1;
+    });
+
+    const importAST = [];
+    for (const [componentName, filePath] of componentsMap.entries()) {
+      importAST.push(createImportAST(filePath, componentName));
+    }
+
+    tree.children = [...importAST, ...tree.children];
+
+    return tree;
+  };
+}
+
+module.exports.remarkCodeDemo = remarkCodeDemo;
